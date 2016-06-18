96c85a1caf104274470b124c9cad0e9de11ebe63
Viktor Kozlovets <work.kozlovets@gmail.com>
2016-06-18 23:20:43 +0300

    some changes

---
+++ Assets/Scripts/Desert/CanvasControl.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasControl : MonoBehaviour
+{
+    public GameObject gameMusic, musicButton, restartButton, quitButton, moveButton, highscore, player;
+    static float musicVolume = 1.0f;
+    public bool paused = false;
+    int jumpTimes = 1;
+    int avalaibleJumps;
+
+    void Awake()
+    {
+        PlayerPrefs.GetFloat("musicVolume", musicVolume);
+        gameMusic.GetComponent<AudioSource>().volume = musicVolume;
+        avalaibleJumps = (PlayerPrefs.GetInt("highscore", avalaibleJumps) / 50) + 2;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        paused = false;
+        Time.timeScale = 1;
+    }
+
+    public void MusicControl()
+    {
+        if (gameMusic.GetComponent<AudioSource>().volume == 0)
+        {
+            musicVolume = 1.0f;
+            gameMusic.GetComponent<AudioSource>().volume = musicVolume;
+            PlayerPrefs.SetFloat("musicVolume", musicVolume);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            musicVolume = 0.0f;
+            gameMusic.GetComponent<AudioSource>().volume = musicVolume;
+            PlayerPrefs.SetFloat("musicVolume", musicVolume);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public void Pause()
+    {
+        paused = !paused;
+        if (paused)
+        {
+            Time.timeScale = 0;
+            musicButton.SetActive(true);
+            restartButton.SetActive(true);
+            quitButton.SetActive(true);
+            highscore.SetActive(true);
+            moveButton.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            musicButton.SetActive(false);
+            restartButton.SetActive(false);
+            quitButton.SetActive(false);
+            highscore.SetActive(false);
+            moveButton.SetActive(true);
+        }
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1;
+    }
+
+    public void Move()
+    {
+        if (PlayerController.grounded == true || jumpTimes < avalaibleJumps)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 400f));
+            jumpTimes++;
+            if (PlayerController.grounded == true)
+                jumpTimes = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public Text highscoreText;
+    public GameObject levelButton, aboutButton, desertButton;
+
+    int highscore = 0;
+    bool about = false;
+    bool choseLevel = false;
+
+    void Awake()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+        highscoreText.text = "Best:\n" + highscore.ToString();
+        aboutButton.GetComponent<Text>().text = "Beat your record and recieve aditional jumps!\nJumps = best / 50 + 2 = " + (highscore/50+2).ToString();
+    }
+
+    public void DesertButton()
+    {
+        SceneManager.LoadScene("Desert");
+    }
+
+    public void ChoseLevel()
+    {
+        if (choseLevel == false)
+        {
+            aboutButton.SetActive(false);
+            desertButton.SetActive(true);
+        }
+        else if(choseLevel == true)
+        {
+            aboutButton.SetActive(true);
+            desertButton.SetActive(false);
+        }
+        choseLevel = !choseLevel;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
